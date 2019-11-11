666c906e6fda6e6c8558ca5911347f9a25974644
NeverSeeG <619894189@qq.com>
2019-11-11 17:03:32 +0800

    海掏项目前台

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div >
+    <h2>Home Page</h2>
+    <el-button type="primary" @click="testAxios()">测试axios调用</el-button>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'Home',
+    methods: {
+       testAxios() {
+           axios.get('http://localhost:8686/goods/findAll').then(function(res){
+               alert(res)
+           }).catch(function(res){
+               alert(res);
+           })
+       } 
+    }
+  }
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div >
+    <h2>Login Page</h2>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
